4a1da806d36954a5c90163fb36f4f42a419bce05
Lumaa <nathan@ama49.fr>
2023-01-12 09:48:03 +0100

    about the bot

---
+++ commands/info.js
@@ -0,0 +1,35 @@
+const { ApplicationCommand, ChatInputCommandInteraction, EmbedBuilder, ActionRowBuilder } = require("discord.js")
+
+module.exports = {
+    /**@type {ApplicationCommand} */
+    data: {
+        name: "info",
+        description: "Obtenez des informations sur Para",
+    },
+    /** 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    execute(interaction) {
+        const pages = [
+            new EmbedBuilder()
+            .setTitle("Général")
+            .setDescription("Ce bot de protection à été fait à la base pour être un simple de bot de modération, par la suite, il deviendra ce qu'il est aujourd'hui : Un bot qui protège ses utilisateurs contre les raids et grief.\nD'autres commandes diverses fut créées pendant le processus.\n\nPar [Lumaa](https://lumination.brebond.com)"),
+            new EmbedBuilder()
+            .setTitle("Confidentialité")
+            .setDescription("Para utilise plusieurs évènements simultanément pour pouvoir empêcher un raid ou un grief, les informations prises en général sont :\n- Le premier salon textuel privé du serveur pour contacter les modérateurs\n\nLes informations prises pour un anti-raid sont :\n- Le nombre de nouveaux arrivants toutes les 10 secondes\n- Le nom d'utilisateur et l'identifiant des nouveaux arrivants\n- Les invitations\n\nLes informations prises pour un anti-grief sont :\n- Les rôles modifiable par Para\n- Les salons supprimés toutes les 10 secondes\n\nToutes les informations que nous utilisons pour vous protéger sont **directement supprimés** même si il ne se passe rien d'alarmant ou si une action à dû être faite."),
+            new EmbedBuilder()
+            .setTitle("Open-Source")
+            .setDescription("Le programme de Para est disponible à tous depuis le mois de janvier sur GitHub :\nhttps://github.com/u-lumaa/Para")
+            .setURL("https://github.com/u-lumaa/Para")
+        ]
+
+        for (let i = 0; i < pages.length; i++) {
+            const page = pages[i];
+            page
+            .setFooter({ text: `${i + 1}/${pages.length} - Par Lumaa#4480` })
+            .setColor("Blue")
+        }
+
+        interaction.reply({ embeds: pages })
+    }
+}
