5eb144c64f59b73f5ea36d3b5bbfb2948cb595ab
Lumaa <nathan@ama49.fr>
2023-07-16 15:55:10 +0200

    confirmation message

--- commands/clear.js
+++ commands/clear.js
@@ -1,4 +1,4 @@
-const { PermissionFlagsBits, ApplicationCommandOptionType, EmbedBuilder } = require("discord.js");
+const { PermissionFlagsBits, ApplicationCommandOptionType, EmbedBuilder, ChatInputCommandInteraction, GuildChannel } = require("discord.js");
 
 module.exports = {
     /**@type {ApplicationCommand} */
@@ -22,10 +22,14 @@ module.exports = {
 			usage: "/clear [channel]*",
 		},
 	},
+    /**
+     * @param {ChatInputCommandInteraction} interaction 
+     */
     async execute(interaction) {
         if (interaction.member.permissions.has([PermissionFlagsBits.ManageChannels])) {
             const _channel =
                 interaction.options.get("channel")?.channel ?? interaction.channel;
+            /**@type {import("discord.js").TextBasedChannel} */
             const clearedChannel = await _channel.clone({
                 position: _channel.rawPosition,
             });
@@ -40,7 +44,12 @@ module.exports = {
                     }> <t:${timestamp.toFixed(0)}:R>`
                 )
                 .setColor("Random");
-            clearedChannel.send({ embeds: [newChannelEmbed] });
+            clearedChannel.send({ embeds: [newChannelEmbed] }).then(() => {
+                if (_channel !== interaction.channel) {
+                    interaction.reply({ content: "Votre salon a été vidé avec succès.", ephemeral: true })
+                }
+            });
+
         }
     }
 }
